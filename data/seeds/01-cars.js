ac6a33dee48bbb3c7e07ff565e96e085107d1610
AbdelI <homeworkoracle@protonmail.com>
2020-01-21 18:20:41 +0100

    seed updated,all endpoints tested, FINISHED: MVP & 2 STRETCHES

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,13 +1,26 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("cars")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("cars").insert([
+        {
+          name: "Aston",
+          VIN: 933,
+          make_model: "2019",
+          mileage: 55,
+          transmission_type: "automatic",
+          status_of_title: "new"
+        },
+        {
+          name: "Ferrari",
+          VIN: 2289,
+          make_model: "2018",
+          mileage: 130,
+          transmission_type: "automatic",
+          status_of_title: "new"
+        }
       ]);
     });
 };
